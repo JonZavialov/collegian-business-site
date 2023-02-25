5532400d3863b0d9d0759651d88a8355743a89ee
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2023-02-25 00:43:33 -0500

    removed hero img

--- src/components/MainHero.js
+++ src/components/MainHero.js
@@ -1,4 +1,3 @@
-import newspaper from '../assets/newspaper.png';
 import '../styles/hero.css'
 import scroll from '../assets/scroll.png'
 
@@ -6,7 +5,6 @@ function MainHero(){
     return (
         <div id="hero">
             <h1>STREET TEAM</h1>
-            <img src={newspaper} alt="Newspaper Graphic"></img>
             <img src={scroll} alt="Scroll Arrow" id="scroll-arrow"></img>
         </div>
     )
