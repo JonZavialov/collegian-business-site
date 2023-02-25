8bc95b45236d177e25bc3bd14b7a772a26199532
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2023-02-25 00:39:22 -0500

    added scroll arrow

--- src/components/MainHero.js
+++ src/components/MainHero.js
@@ -1,11 +1,13 @@
 import newspaper from '../assets/newspaper.png';
 import '../styles/hero.css'
+import scroll from '../assets/scroll.png'
 
 function MainHero(){
     return (
         <div id="hero">
             <h1>STREET TEAM</h1>
             <img src={newspaper} alt="Newspaper Graphic"></img>
+            <img src={scroll} alt="Scroll Arrow" id="scroll-arrow"></img>
         </div>
     )
 }
