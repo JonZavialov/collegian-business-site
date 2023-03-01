5d6c5daad11a01b4dbff13bd92cb6753e700d242
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2023-03-01 09:11:18 -0500

    added some more features to dashboard cards

--- src/components/dashboards/DashboardCard.js
+++ src/components/dashboards/DashboardCard.js
@@ -1,9 +1,14 @@
 import getDashboardImg from "../../utils/getDashboardImg";
+import dashboards from '../../assets/text/dashboards.json';
+import redirectToDashboard from "../../utils/redirectToDashboard";
 
 function DashboardCard(props){
     return (
         <div className = "dashboard-card">
             <img src={getDashboardImg()} alt={props.title + " dashboard preview"} ></img>
+            <h2>{props.title}</h2>
+            <p>{dashboards[props.title].description}</p>
+            <button onClick={() => redirectToDashboard(props.title)}>Open</button>
         </div>
     )
 }
---
+++ src/utils/redirectToDashboard.js
@@ -0,0 +1,8 @@
+function redirectToDashboard(title){
+    // check if title exists within json
+    // if so, redirect to new URL
+    // otherwise returh
+    console.log(`redirecting to ${title}`)
+}
+
+export default redirectToDashboard;
